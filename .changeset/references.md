18d405b93a055aa55839a1fc9cd6beb0555bd7d0
Isha Dijcks <ishadijcks@gmail.com>
2026-05-20 17:28:20 +0200

    Add references to link schemas to each other

---
+++ .changeset/references.md
@@ -0,0 +1,5 @@
+---
+'@123ishatest/louter': minor
+---
+
+Add references to link schemas to each other
--- src/validator/LouterValidator.ts
+++ src/validator/LouterValidator.ts
@@ -3,12 +3,19 @@ import type { KindDefinitions } from '@louter/core/types';
 import type { LouterStage } from '@louter/core/LouterStage';
 import type { LouterContext } from '@louter/core/LouterContext';
 import { LouterWarningType } from '@louter/core/LouterWarningType';
+import { flattenObject } from 'es-toolkit';
+import { LOUTER_REFERENCE_PREFIX, LOUTER_SEPARATOR } from '@louter/core/references';
 
 /**
  * Validate all LouterObjects through their Zod schemas
  */
 export class LouterValidator implements LouterStage {
   run<Kinds extends KindDefinitions>(ctx: LouterContext<Kinds>): void {
+    this.parse(ctx);
+    this.validateReferences(ctx);
+  }
+
+  parse<Kinds extends KindDefinitions>(ctx: LouterContext<Kinds>): void {
     ctx.objects.forEach((object) => {
       const schema = ctx.kinds[object.kind];
       if (!schema) {
@@ -51,6 +58,40 @@ export class LouterValidator implements LouterStage {
       // TODO(@Isha): Fix?
       // @ts-expect-error Fix map already existing
       ctx.content[object.kind][id] = zodResult.data;
+    });
+  }
+
+  validateReferences<Kinds extends KindDefinitions>(ctx: LouterContext<Kinds>): void {
+    Object.values(ctx.content).forEach((items) => {
+      Object.values(items).forEach((item) => {
+        const flat = flattenObject(item as object);
+
+        Object.entries(flat).forEach(([key, value]) => {
+          if (value.toString().startsWith(LOUTER_REFERENCE_PREFIX)) {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const [_, kind, ...rest] = value.split(LOUTER_SEPARATOR);
+            const refId = rest.join(LOUTER_SEPARATOR);
+
+            if (!ctx.content[kind]) {
+              ctx.warnings.push({
+                type: LouterWarningType.MissingReferenceKind,
+                message: `Missing reference kind '${kind}'`,
+                path: key,
+              });
+              return;
+            }
+
+            if (!ctx.content[kind][refId]) {
+              ctx.warnings.push({
+                type: LouterWarningType.MissingReference,
+                message: `Missing reference '${value}'`,
+                path: key,
+              });
+              return;
+            }
+          }
+        });
+      });
     });
   }
 }
